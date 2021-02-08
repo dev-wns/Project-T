dd87ea0fdf7ecc584a1867f3c95286331f0b9b8f
wnsghks <ashi0@naver.com>
2021-02-08 13:35:53 +0900

    [플레이어] Run Animation 적용

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    const float speed = 100.0f;
+    public Animator anim;
+
+    private const float speed = 200.0f;
+    private PlayerState state = PlayerState.Idle;
+
+    private enum PlayerState
+    {
+        Idle = 0,
+        Run,
+    }
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        state = PlayerState.Idle;
         if ( Input.GetKey( KeyCode.W ) )
         {
             transform.Translate( Vector3.up * speed * Time.deltaTime );
+            state = PlayerState.Run;
         }
 
         if ( Input.GetKey( KeyCode.S ) )
         {
             transform.Translate( Vector3.down * speed * Time.deltaTime );
+            state = PlayerState.Run;
         }
 
         if ( Input.GetKey( KeyCode.D ) )
         {
             transform.Translate( Vector3.right * speed * Time.deltaTime );
+            state = PlayerState.Run;
         }
 
         if( Input.GetKey( KeyCode.A ) )
         {
             transform.Translate( Vector3.left * speed * Time.deltaTime );
+            state = PlayerState.Run;
         }
+
+        anim.SetInteger( "State", ( int )state );
     }
 }
